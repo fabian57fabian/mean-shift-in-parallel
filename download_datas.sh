25d3ee54e4229200a90e302cbfe95dc12ea8a0c5
AngeloDamante <angelo.damante16@gmail.com>
2021-05-10 23:44:55 +0200

    download_data.sh modified to handle datas.tar.gz file

--- download_datas.sh
+++ download_datas.sh
@@ -1,10 +1,14 @@
 #/bin/bash
 WORKING_DIR="datas"
+TMP_FILE="datas.tar.gz"
 echo "Removing temporany datas"
 if [ -d "$WORKING_DIR" ]; then rm $WORKING_DIR -r; fi
+if [ -d "$TMP_FILE" ]; then rm $TMP_FILE -r; fi
 mkdir datas
 echo "Downloading datas"
 wget https://github.com/fabian57fabian/mean-shift-in-parallel/releases/download/0.1/datas.tar.gz
 echo "Extracting datas"
-tar -zxvf datas.tar.gz -C datas/
+tar -zxvf $TMP_FILE -C datas/
+rm $TMP_FILE
+if [ -d "$TMP_FILE" ]; then rm $TMP_FILE -r; fi
 echo "Done"
