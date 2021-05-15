50afb1981ee048fa165df387e33d4573eb677f1b
fabian57fabian <fabian_57@yahoo.it>
2021-05-15 15:28:01 +0200

    Changed download datas scirpt for compatibility for pre-release 0.3

--- download_datas.sh
+++ download_datas.sh
@@ -6,9 +6,9 @@ if [ -d "$WORKING_DIR" ]; then rm $WORKING_DIR -r; fi
 if [ -d "$TMP_FILE" ]; then rm $TMP_FILE -r; fi
 mkdir datas
 echo "Downloading datas"
-wget https://github.com/fabian57fabian/mean-shift-in-parallel/releases/download/0.1/datas.tar.gz
+wget https://github.com/fabian57fabian/mean-shift-in-parallel/releases/download/0.3/datas.tar.gz
 echo "Extracting datas"
-tar -zxvf $TMP_FILE -C datas/
+tar -zxvf $TMP_FILE -C ./
 rm $TMP_FILE
 if [ -d "$TMP_FILE" ]; then rm $TMP_FILE -r; fi
 echo "Done"
