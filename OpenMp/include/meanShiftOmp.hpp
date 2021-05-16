42b891c1511371192f1db214634c84d351802be9
AngeloDamante <angelo.damante16@gmail.com>
2021-05-17 00:39:48 +0200

    reformat code

--- OpenMp/include/meanShiftOmp.hpp
+++ OpenMp/include/meanShiftOmp.hpp
@@ -15,13 +15,60 @@ namespace ms
         namespace stat
         {
             template <typename T, const size_t N, const size_t D>
-            std::vector<utils::vec<T, D>> cluster_points(utils::mat<T, N, D> &data, const size_t niter, const float bandwidth, const float radius, const float min_distance, const size_t num_threads)
+            std::vector<utils::vec<T, D>> cluster_points(utils::mat<T, N, D> &data, const size_t niter, \
+            float bandwidth, const float radius, const float min_distance, const size_t num_threads)
             {
                 const float double_sqr_bdw = 2 * bandwidth * bandwidth;
                 utils::mat<T, N, D> new_data;
                 for (size_t i = 0; i < niter; ++i) // number_iterations
                 {
-                    #pragma omp parallel for default(none) shared(data, niter, bandwidth, radius, double_sqr_bdw, new_data) schedule(static) num_threads(num_threads)
+                    #pragma omp parallel for default(none) \ 
+                    shared(data, niter, bandwidth, radius, double_sqr_bdw, new_data) \ 
+                    schedule(static) num_threads(num_threads)
+                    for (size_t p = 0; p < N; ++p)
+                    {
+                        utils::vec<T, D> new_position{};
+                        float sum_weights = 0.;
+                        for (size_t q = 0; q < N; ++q)
+                        {
+                            double distance = geometricFunction::calc_distance(data[p], data[q]);
+                            if (distance <= radius)
+                            {
+                                float gaussian = std::exp(-distance / double_sqr_bdw);
+                                new_position = new_position + data[q] * gaussian;
+                                // new_position = utils::op::operator+(new_position, utils::op::operator*(data[q], gaussian));
+                                sum_weights += gaussian;
+                            }
+                        }
+                        
+                        /*
+                         * Diversi thread scrivono in diverse posizioni di
+                         * memoria. Come buona pratica, lo tengo? 
+                        */
+                        #pragma omp critical [compute_m]
+                        new_data[p] = new_position/sum_weights;
+                        // new_data[p] = utils::op::operator/(new_position, sum_weights);
+                    }
+                    data = new_data;
+                }
+                return geometricFunction::reduce_to_centroids(data, min_distance);
+            }
+
+        } // namespace ms::stat
+
+        namespace dyn
+        {
+            template <typename T, const size_t N, const size_t D>
+            std::vector<utils::vec<T, D>> cluster_points(utils::mat<T, N, D> &data, const size_t niter, \ 
+            const float bandwidth, const float radius, const float min_distance)
+            {
+                const float double_sqr_bdw = 2 * bandwidth * bandwidth;
+                utils::mat<T, N, D> new_data;
+                for (size_t i = 0; i < niter; ++i)
+                {
+                    #pragma omp parallel for default(none) \ 
+                    shared(data, niter, bandwidth, radius, double_sqr_bdw, new_data) \ 
+                    schedule(dynamic) 
                     for (size_t p = 0; p < N; ++p)
                     {
                         utils::vec<T, D> new_position{};
@@ -38,49 +85,9 @@ namespace ms
                             }
                         }
                         
-                        /*
-                         * Diversi thread scrivono in diverse posizioni di
-                         * memoria. Come buona pratica, lo tengo? 
-                        */
                         #pragma omp critical [compute_m]
-                        new_data[p] = utils::op::operator/(new_position, sum_weights);
-                        // new_data[p] = new_position/sum_weights;
-                    }
-                    data = new_data;
-                }
-                return geometricFunction::reduce_to_centroids(data, min_distance);
-            }
-
-        } // namespace ms::stat
-
-        namespace dyn
-        {
-            template <typename T, const size_t N, const size_t D>
-            std::vector<utils::vec<T, D>> cluster_points(utils::mat<T, N, D> &data, const size_t niter, const float bandwidth, const float radius, const float min_distance)
-            {
-                const float double_sqr_bdw = 2 * bandwidth * bandwidth;
-                utils::mat<T, N, D> new_data;
-                for (size_t i = 0; i < niter; ++i)
-                {
-                    #pragma omp parallel for default(none) shared(data, niter, bandwidth, radius, double_sqr_bdw, new_data) schedule(dynamic) 
-                    for (size_t p = 0; p < N; ++p)
-                    {
-                        utils::vec<T, D> new_position{};
-                        float sum_weights = 0.;
-                        for (size_t q = 0; q < N; ++q)
-                        {
-                            double dist = geometricFunction::calc_distance(data[p], data[q]);
-                            if (dist <= radius)
-                            {
-                                float gaussian = std::exp(-dist / double_sqr_bdw);
-                                // new_position = new_position + data[q] * gaussian;
-                                new_position = utils::op::operator+(new_position, utils::op::operator*(data[q], gaussian));
-                                sum_weights += gaussian;
-                            }
-                        }
-                        
-                        #pragma omp critical
-                        new_data[p] = utils::op::operator/(new_position, sum_weights);
+                        new_data[p] = new_position/sum_weights;
+                        // new_data[p] = utils::op::operator/(new_position, sum_weights);   
                     }
                     data = new_data;
                 }
