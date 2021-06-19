9f2db3ab37eada3a03835e9874c5bc26a7bbd6ed
fabian57fabian <fabain_57@yahoo.it>
2021-06-19 11:41:33 +0200

    Changed indexing for results (DATASET|TILE_WIDTH)

--- CUDA/MS_CUDA/create_all_timings.sh
+++ CUDA/MS_CUDA/create_all_timings.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-echo "THREADS,POINTS_NUMBER,NAIVE,SHARED"
+echo "POINTS_NUMBER,TILE_WIDTH,NAIVE,SHARED"
 #dirs=( $( ls ../../datas ) )
 dirs=( $( ls /MS_CUDA/datas ) )
-for thr in 4 8 16 32 64 128 256 512 1024
-do
 for dir in "${dirs[@]}"
+do
+for thr in 8 16 32 64 128 256 512 1024
 do
 	echo "#ifndef THREAD_SETTINGS
 #define THREAD_SETTINGS
@@ -15,8 +15,8 @@ const int POINTS_NUMBER = "$dir";
 }
 #endif" > thread_settings.h
     ./build.sh &> /dev/null
-    printf $thr","
     printf $dir","
+    printf $thr","
     ./execute_mean_shift.sh info
 done
 done
